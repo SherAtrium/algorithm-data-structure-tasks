7a70569cf55410015133b2f3b0da6a4280455f8e
Sheratrium <sherzodm214@gmail.com>
2024-07-20 12:08:22 +0500

    feat: setup project

---
+++ jest.config.js
@@ -0,0 +1,16 @@
+/** @type {import('ts-jest').JestConfigWithTsJest} **/
+export default {
+  preset: "ts-jest",
+  testEnvironment: "jsdom",
+  rootDir: "./",
+  coverageDirectory: "<rootDir>/coverage",
+  collectCoverageFrom: [
+      "<rootDir>/src/**/*.ts",
+  ],
+  testPathIgnorePatterns: ["<rootDir>/node_modules/"],
+  coverageReporters: ["json", "html"],
+  testMatch: ["<rootDir>/src/**/test.ts"],
+  transform: {
+    "^.+.tsx?$": ["ts-jest",{}],
+  },
+};
---
+++ src/stringReversal/index.ts
@@ -0,0 +1,11 @@
+// Task
+// Return a new string with the reversed
+
+// Examples
+//   reverse('apple') === 'leppa'
+//   reverse('hello') === 'olleh'
+//   reverse('Happy!') === '!yppaH'
+
+export const reverse = (str: string): string => {
+  return '';
+}
