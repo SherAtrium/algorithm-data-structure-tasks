f6b9aad5ddc3c5c774bf36606284007ccca95055
Sheratrium <sherzodm214@gmail.com>
2024-07-25 14:08:03 +0500

    upd: maxChars

--- src/4_maxChars/index.ts
+++ src/4_maxChars/index.ts
@@ -7,31 +7,19 @@
 
 
 // #1 Solution
-// export const maxChar = (str: string): string => {
-//   const charsObj: Record<string, number> = {};
+export const maxChar = (str: string): string => {
+  const charsObj: Record<string, number> = {};
 
-//   for (let char of str) {
-//     charsObj[char] = charsObj[char] + 1 || 1;
-//   }
+  for (let char of str) {
+    charsObj[char] = charsObj[char] + 1 || 1;
+  }
 
-//   const sortedEntries = Object.entries(charsObj).sort((a, b) => a[1] - b[1]);
+  const sortedEntries = Object.entries(charsObj).sort((a, b) => a[1] - b[1]);
 
-//   return sortedEntries[sortedEntries.length - 1][0];
-// }
+  return sortedEntries[sortedEntries.length - 1][0];
+}
 
-
-// max = 0;
-/* 
- 'hello'
- {
-  h: 1,
-  e: 1,
-  l: 2,
-  o: 1,
- }
-*/
-
-
+// #2 Solution
 export const maxChar = (str: string): string => {
   const charsObj: Record<string, number> = {};
   let max: number = 0;
